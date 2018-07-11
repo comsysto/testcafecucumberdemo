0dea59ed31532cc099fd351fbf06d52aa2450381
Renato Curic <r.curic@reply.de>
2018-07-12 01:14:02 +0200

    Improved code, moved metadata to json file. es5->es6

--- src/reporters/html/prepareJson.js
+++ src/reporters/html/prepareJson.js
@@ -1,70 +1,41 @@
-var osControl = require('os');
-var fsControl = require('fs');
-var pathFileNameChrome = "reports/chrome.json";
-var pathFileNameFirefox = "reports/firefox.json";
-var jsonChrome = require('../../../' + pathFileNameChrome);
-var jsonFirefox = require('../../../' + pathFileNameFirefox);
-var osName = osControl.platform();
-var osRelease = osControl.release();
+const osControl = require('os');
+const fsControl = require('fs');
+const pathFileNameChrome = "reports/chrome.json";
+const pathFileNameFirefox = "reports/firefox.json";
+const jsonChrome = require('../../../' + pathFileNameChrome);
+const jsonFirefox = require('../../../' + pathFileNameFirefox);
+const browserMetadata = require('./metadata.json');
+let osName = osControl.platform();
+let osRelease = osControl.release();
 
 if (osName === 'darwin') {
     osName = 'osx'
 }
-
-var device = 'Local test machine';
-
-var platform = {
+let platformData = {
     name: osName,
     version: osRelease
 };
 
-var firefoxVersion = {
-    name: 'firefox',
-    version: '60.0.2'
-};
-
-var chromeVersion = {
-    name: 'chrome',
-    version: '67.0.33'
-};
-
-var metadataFirefox = {
-    browser: firefoxVersion,
-    device: device,
-    platform: platform
-};
-
-var metadataChrome = {
-    browser: chromeVersion,
-    device: device,
-    platform: platform
-};
+browserMetadata.metadataChrome.platform=platformData;
+browserMetadata.metadataFirefox.platform=platformData;
 
 function prepareJson() {
 
-    // TODO improve code
-    // TODO improve reporter so it checks the browser version
-    // TODO check if TestCafe can inject metadata for cucumberJS report
-
-    for (var i = 0; i < jsonChrome.length; i++) {
-        jsonChrome[i].metadata = metadataChrome;
+    for (let index of jsonChrome){
+        index.metadata = browserMetadata.metadataChrome;
     }
-
-    for (var i = 0; i < jsonFirefox.length; i++) {
-        jsonFirefox[i].metadata = metadataFirefox;
+    for (let index of jsonFirefox){
+        index.metadata = browserMetadata.metadataFirefox;
     }
-
     fsControl.writeFile(pathFileNameChrome, JSON.stringify(jsonChrome, null, 2), 'utf-8', function (err) {
         if (err) {
             return console.log(err);
         }
     });
-
     fsControl.writeFile(pathFileNameFirefox, JSON.stringify(jsonFirefox, null, 2), 'utf-8', function (err) {
         if (err) {
             return console.log(err);
         }
     });
 }
-
 prepareJson();
