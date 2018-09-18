7741332fcd81a49b3122ed6337317d9f4ec7e7ab
Renato Curic <r.curic@reply.de>
2018-09-19 00:07:22 +0200

    Refactor to use page objects

---
+++ features/step_definitions/ComsystoBlog.js
@@ -0,0 +1,90 @@
+const { Given, When, Then } = require('cucumber');
+const { Selector, ClientFunction } = require('testcafe');
+const Page = require('./pageObjects/ComsystoBlog');
+const page = new Page();
+let testController = null;
+
+const getWindowLocation = ClientFunction(function () {
+  return window.location
+});
+
+/*
+In this section we are testing the Comsysto Reply web page.
+ */
+
+Given('Ana wants to view the list of all Comsysto Reply blog posts', function () {
+  const parameters = this.parameters;
+
+  return this.waitForTestController().then(function (tc) {
+    testController = tc;
+
+    return testController
+      .navigateTo(parameters.comsystoReplyUrl);
+  });
+});
+
+When('she clicks the Blog post link', function () {
+  const blogButton = page.home.blogButton.with({ boundTestRun: testController }).withText('Blog');
+
+  return testController
+    .click(blogButton);
+});
+
+Then('she should see the list of all blog posts', function () {
+  const htmlBlogListExists = page.blog.frame.with({ boundTestRun: testController }).exists;
+
+  return testController
+    .expect(htmlBlogListExists).ok();
+});
+
+Given('Ana is on the list of all Comsysto Reply blog posts', function () {
+  const parameters = this.parameters;
+  const blogButton = page.home.blogButton.with({ boundTestRun: testController }).withText('Blog');
+
+  return this.waitForTestController().then(function (tc) {
+    testController = tc;
+
+    return testController
+      .navigateTo(parameters.comsystoReplyUrl)
+      .click(blogButton);
+  });
+});
+
+When('she clicks the Open Nebula blog post', function () {
+  const link = page.blog.link.with({ boundTestRun: testController }).withText('PRIVATE CLOUD INFRASTRUCTURE WITH OPENNEBULA');
+
+  return testController
+    .click(link);
+});
+
+Then('she should see the Open Nebula blog post', function () {
+  const blogContent = page.blog.frame.with({ boundTestRun: testController }).exists;
+
+  const getLocation = getWindowLocation.with({ boundTestRun: testController });
+  getLocation().then(function (locationData) {
+
+    return testController
+      .expect(blogContent).ok()
+      .expect(locationData.href).contains("private-cloud-infrastructure-with-opennebula");
+  });
+
+});
+
+When('she clicks the TestCafe blog post', function () {
+  const link = page.blog.link.with({ boundTestRun: testController }).withText('TEST AUTOMATION WITH TESTCAFE AND CUCUMBER.JS');
+
+  return testController
+    .click(link);
+});
+
+Then('she should see the TestCafe blog post', function () {
+  const blogContent = page.blog.frame.with({ boundTestRun: testController }).exists;
+
+  const getLocation = getWindowLocation.with({ boundTestRun: testController });
+  getLocation().then(function (locationData) {
+
+    return testController
+      .expect(blogContent).ok()
+      .expect(locationData.href).contains("test-automation-with-testcafe-and-cucumber.js");
+  });
+});
---
+++ features/step_definitions/pageObjects/ComsystoBlog.js
@@ -0,0 +1,26 @@
+const { Selector } = require('testcafe');
+
+var testController = null;
+
+class Home {
+  constructor() {
+    this.blogButton = Selector('.nav-link-text-mobile').with({ boundTestRun: testController });
+
+  }
+}
+
+class Blog {
+  constructor() {
+    this.frame = Selector('.w-dyn-list');
+    this.link = Selector('.card.w-dyn-item.w-col.w-col-4').with({ boundTestRun: testController });
+  }
+}
+
+class Page {
+  constructor() {
+    this.home = new Home();
+    this.blog = new Blog();
+  }
+}
+
+module.exports = Page;
