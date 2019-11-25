4a2ad518c00f40191e0cebf57a9d72bb1152b643
Tomislav Gajsak <tomislav.gajsak@comsysto.com>
2019-11-25 20:58:10 +0100

    Generate html report for comsystoJob task

--- src/reporters/html/prepareJson.js
+++ src/reporters/html/prepareJson.js
@@ -3,7 +3,7 @@ const fsControl = require('fs');
 const pathFileNameChrome = "reports/Chrome.json";
 const pathFileNameFirefox = "reports/Firefox.json";
 const jsonChrome = require('../../../' + pathFileNameChrome);
-const jsonFirefox = require('../../../' + pathFileNameFirefox);
+//const jsonFirefox = require('../../../' + pathFileNameFirefox);
 const browserMetadata = require('./metadata.json');
 let osName = osControl.platform();
 let osRelease = osControl.release();
@@ -24,19 +24,19 @@ function prepareJson() {
     for (let index of jsonChrome) {
         index.metadata = browserMetadata.metadataChrome;
     }
-    for (let index of jsonFirefox) {
+  /*  for (let index of jsonFirefox) {
         index.metadata = browserMetadata.metadataFirefox;
-    }
+    }*/
     fsControl.writeFile(pathFileNameChrome, JSON.stringify(jsonChrome, null, 2), 'utf-8', function (err) {
         if (err) {
             return console.log(err);
         }
     });
-    fsControl.writeFile(pathFileNameFirefox, JSON.stringify(jsonFirefox, null, 2), 'utf-8', function (err) {
+   /* fsControl.writeFile(pathFileNameFirefox, JSON.stringify(jsonFirefox, null, 2), 'utf-8', function (err) {
         if (err) {
             return console.log(err);
         }
-    });
+    });*/
 }
 
 prepareJson();
